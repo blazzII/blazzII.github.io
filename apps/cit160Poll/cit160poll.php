f3bebb1439a0113e7d9f9c95fb5bda204750960c
Blazzard <blazzard@gmail.com>
2020-12-05 07:12:47 -0700

    d

--- apps/cit160Poll/cit160poll.php
+++ apps/cit160Poll/cit160poll.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <title>CIT 160 Registration Poll</title>
-  <?php include '../../includes/head.php'; ?>
+  <?php include 'includes/head.php'; ?>
   <link href="styles/phpform.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
 </head>
@@ -10,14 +10,14 @@
 <body>
  <div class="wrapper">
   <header>
-    <?php include '../../includes/header-byui.php'; ?> 
+    <?php include 'includes/header.php'; ?>
   </header>
   <section class="supercenter">
     <h2>What is your CIT 160 experience?</h2>
     <div class="instructions">CIT 160 is an introduction to programming course using JavaScript at BYU-I</div>
    	<form action="cit160pollwrite.php" method="post" class="success">
 	    <label for="fullName">Enter your full name: </label>
-      <input type="text" name="fullName" placeholder="Full Name" size="40" required>
+      <input type="text" name="fullName" placeholder="Full Name" size="40" required patter="[A-Za-z -]*">
    	  <fieldset>
         <label for="cit160">Which of the following applies to you?</label><br>
         <input type="radio" name="cit160" value="completed" required> Completed CIT 160<br>
@@ -26,12 +26,12 @@
         <input type="radio" name="cit160" value="none"> Not completed nor am I currently enrolled in CIT 160
       </fieldset>
       <br>
-      <input type="submit" value="Submit Poll"> 
+      <input type="submit" value="Submit Poll">
   	</form>
     <div class="clear"> </div>
-  </section>  
+  </section>
   <footer>
-    <?php include '../../includes/footer.php'; ?>
+    <?php include 'includes/footer.php'; ?>
   </footer>
  </div>
 </body>

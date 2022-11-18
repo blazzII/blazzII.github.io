39df54574d22481c30107d1d112c8e581c2e6aa4
Blazzard <blazzard@gmail.com>
2022-11-18 16:15:16 -0700

    open graph additions

---
+++ js/turnItUpsideDown/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const smileButton = document.querySelector('#smile');
+  const frownButton = document.querySelector('#frown');
+  const face = document.querySelector('#face');
+
+  
+
+
+});
